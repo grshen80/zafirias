e2ee2c0657f61f5e94c78279b313b3e422758319
grshen80 <grshen80@gmail.com>
2018-06-04 22:17:15 -0700

    Minor bug fix to change forms to models

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django import forms
 from django.contrib.auth.models import User
 
 CHARACTERS = (
@@ -53,33 +52,33 @@ class Member(models.Model):
 
 
 class Character(models.Model):
-    name = forms.CharField(max_length=64, choices=CHARACTERS)
-    tagline = forms.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64, choices=CHARACTERS)
+    tagline = models.CharField(max_length=64, unique=True)
     image = models.BinaryField(blank=True)
-    type = forms.CharField(max_length=64, choices=TYPES)
-    secondary_type = forms.CharField(max_length=64, choices=TYPES, null=True)
-    weapon_type = forms.CharField(max_length=64, choices=WEAPONS)
-    base_hp = forms.IntegerField()
-    base_atk = forms.IntegerField()
-    base_spd = forms.IntegerField()
-    base_def = forms.IntegerField()
-    base_res = forms.IntegerField()
-    base_atk_name = forms.CharField(max_length=64)
-    base_def_name = forms.CharField(max_length=64)
-    base_ult_name = forms.CharField(max_length=64)
+    type = models.CharField(max_length=64, choices=TYPES)
+    secondary_type = models.CharField(max_length=64, choices=TYPES, null=True)
+    weapon_type = models.CharField(max_length=64, choices=WEAPONS)
+    base_hp = models.IntegerField()
+    base_atk = models.IntegerField()
+    base_spd = models.IntegerField()
+    base_def = models.IntegerField()
+    base_res = models.IntegerField()
+    base_atk_name = models.CharField(max_length=64)
+    base_def_name = models.CharField(max_length=64)
+    base_ult_name = models.CharField(max_length=64)
 
 
 class Attack(models.Model):
-    name = forms.CharField(max_length=64, choices=ATTACKS)
-    atk = forms.IntegerField()
-    special = forms.CharField(max_length=64)    # any special effects
+    name = models.CharField(max_length=64, choices=ATTACKS)
+    atk = models.IntegerField()
+    special = models.CharField(max_length=64)    # any special effects
 
 
 class Defend(models.Model):
-    name = forms.CharField(max_length=64, choices=DEFENDS)
-    special = forms.CharField(max_length=64)    # any special effects
+    name = models.CharField(max_length=64, choices=DEFENDS)
+    special = models.CharField(max_length=64)    # any special effects
 
 
 class Ultimate(models.Model):
-    name = forms.CharField(max_length=64, choices=ULTIMATES)
-    special = forms.CharField(max_length=64)    # any special effects
+    name = models.CharField(max_length=64, choices=ULTIMATES)
+    special = models.CharField(max_length=64)    # any special effects
